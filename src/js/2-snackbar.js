68e4b1d0d26fecc543a934b804abf5efb72e4057
Anastasia <akroyl419@gmail.com>
2025-07-13 18:50:29 +0300

    task2 fix

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -8,23 +8,28 @@ form.addEventListener("submit", makePromise);
 function makePromise(ev) {
     ev.preventDefault()
     const promise = new Promise((resolve, reject) => {
-        setTimeout(() => { 
+        setTimeout(() => {
             if (form.elements.state.value === "fulfilled") {
-            resolve(iziToast.show({
-                message: `✅ Fulfilled promise in ${form.elements.delay.value}ms`,
-                backgroundColor: "#59A10D",
-                messageColor: "#fff",
-                position: "topRight",
-            }))
+                resolve();
             } else {
-                reject(iziToast.show({
-                message: `❌ Rejected promise in ${form.elements.delay.value}ms`,
-                backgroundColor: "#EF4040",
-                messageColor: "#fff",
-                position: "topRight",
-            }))
+                reject();
             }
         }, form.elements.delay.value)
-        }
-    )
+    });
+    promise.then(() => {
+        iziToast.show({
+            message: `✅ Fulfilled promise in ${form.elements.delay.value}ms`,
+            backgroundColor: "#59A10D",
+            messageColor: "#fff",
+            position: "topRight",
+        });
+    });
+    promise.catch(() => {
+        iziToast.show({
+            message: `❌ Rejected promise in ${form.elements.delay.value}ms`,
+            backgroundColor: "#EF4040",
+            messageColor: "#fff",
+            position: "topRight",
+        });
+    });
 }
